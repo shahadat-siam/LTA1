4e9eb73e321e6005dca1f6b55b05577396c56843
shahadat-siam <lonelinessismycompanion09@gmail.com>
2025-05-08 19:31:52 +0600

    modify

--- a1.ts
+++ a1.ts
@@ -1,7 +1,20 @@
 function formatString (input: string, toUpper: boolean = true ) : string{
     return toUpper ? input.toUpperCase() : input.toLowerCase()
-}
-
+} 
  
 const result = formatString('Hello',false)
 console.log(result)
+
+
+
+function filterByRating(item : {title: string, rating: number}[]): {title: string, rating: number}[] {
+    return item.filter(item => item.rating >= 4)
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+  ];
+
+const rating = filterByRating(books)
+console.log(rating)
